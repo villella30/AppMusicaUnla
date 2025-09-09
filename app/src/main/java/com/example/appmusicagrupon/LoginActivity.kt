6d6a0e5e03d2c4b783bb1531c4728efa2dc61d77
santitorrens1 <santitorrens1@gmail.com>
2025-09-09 00:00:35 -0300

    Implementacion de vista login

---
+++ app/src/main/java/com/example/appmusicagrupon/LoginActivity.kt
@@ -0,0 +1,58 @@
+package com.example.appmusicagrupon
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LoginActivity : AppCompatActivity() {
+    lateinit var Usuario: EditText
+
+    lateinit var Contraseña: EditText
+
+    lateinit var RecordarUsuario: CheckBox
+
+    lateinit var IniciarSesion: Button
+
+    lateinit var CrearUsuario: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        Usuario = findViewById(R.id.Usuario)
+        Contraseña = findViewById(R.id.Contraseña)
+        RecordarUsuario = findViewById(R.id.RecordarUsuario)
+        IniciarSesion = findViewById(R.id.IniciarSesion)
+        CrearUsuario = findViewById(R.id.CrearUsuario)
+
+        CrearUsuario.setOnClickListener {
+            if (Usuario.text.toString().isEmpty() || Contraseña.text.toString().isEmpty()) {
+                Toast.makeText(this, "Por favor, complete los datos", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
+        IniciarSesion.setOnClickListener {
+            if (Usuario.text.toString().isEmpty() || Contraseña.text.toString().isEmpty()) {
+                Toast.makeText(this, "Por favor, complete los datos", Toast.LENGTH_SHORT).show()
+            } else {
+                val intent = Intent(this, MainActivity::class.java)
+                intent.putExtra("Usuario:", Usuario.text.toString())
+                startActivity(intent)
+                finish()
+            }
+
+        }
+    }
+}
