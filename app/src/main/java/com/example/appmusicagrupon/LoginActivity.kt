2bca85f4cc10b14b701bdb0d4ff3304ebd755077
Valentino Villella <111453671+villella30@users.noreply.github.com>
2025-09-28 16:10:49 -0300

    Funcionalidad Recordar Usuario con Shared Preferences

--- app/src/main/java/com/example/appmusicagrupon/LoginActivity.kt
+++ app/src/main/java/com/example/appmusicagrupon/LoginActivity.kt
@@ -13,13 +13,9 @@ import androidx.core.view.WindowInsetsCompat
 
 class LoginActivity : AppCompatActivity() {
     lateinit var Usuario: EditText
-
     lateinit var Contraseña: EditText
-
     lateinit var RecordarUsuario: CheckBox
-
     lateinit var IniciarSesion: Button
-
     lateinit var CrearUsuario: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,12 +29,24 @@ class LoginActivity : AppCompatActivity() {
         Usuario = findViewById(R.id.Usuario)
         Contraseña = findViewById(R.id.Contraseña)
         RecordarUsuario = findViewById(R.id.RecordarUsuario)
+
+        var preferencias = getSharedPreferences(resources.getString(R.string.sp_credenciales), MODE_PRIVATE)
+        var usuarioGuardado = preferencias.getString(resources.getString(R.string.nombre), "")
+        var passwordGuardada = preferencias.getString(resources.getString(R.string.password), "")
+
+        if(usuarioGuardado != "" && passwordGuardada != "")
+            iniciarActividadPrincipal(usuarioGuardado)
+
         IniciarSesion = findViewById(R.id.IniciarSesion)
         CrearUsuario = findViewById(R.id.CrearUsuario)
 
         CrearUsuario.setOnClickListener {
             if (Usuario.text.toString().isEmpty() || Contraseña.text.toString().isEmpty()) {
                 Toast.makeText(this, "Por favor, complete los datos", Toast.LENGTH_SHORT).show()
+
+            }else {
+                iniciarActividadPrincipal(Usuario.text.toString())
+=======
             }else{
                 val intent = Intent(this, TerminosYCondicionesActivity::class.java)
                 startActivity(intent)
@@ -51,11 +59,30 @@ class LoginActivity : AppCompatActivity() {
             if (Usuario.text.toString().isEmpty() || Contraseña.text.toString().isEmpty()) {
                 Toast.makeText(this, "Por favor, complete los datos", Toast.LENGTH_SHORT).show()
             } else {
+
+                login(Usuario.text.toString(), Contraseña.text.toString())
+=======
                 val intent = Intent(this, MainActivity::class.java)
                 startActivity(intent)
                 finish()
+
             }
 
         }
+    }
+
+    private fun login(usuario: String?, password: String?) {
+        if(RecordarUsuario.isChecked) {
+            var preferencias = getSharedPreferences(resources.getString(R.string.sp_credenciales), MODE_PRIVATE)
+            preferencias.edit().putString(resources.getString(R.string.nombre), usuario).apply()
+            preferencias.edit().putString(resources.getString(R.string.password), password).apply()
+        }
+        iniciarActividadPrincipal(Usuario.text.toString())
+    }
+    private fun iniciarActividadPrincipal(usuario: String?) {
+        val intent = Intent(this, MainActivity::class.java)
+        intent.putExtra("NOMBRE", usuario)
+        startActivity(intent)
+        finish()
     }
 }
