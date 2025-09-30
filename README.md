3720d5b8a5efb904eae7bcd227764ca02194c0ef
santitorrens1 <santitorrens1@gmail.com>
2025-09-30 10:21:35 -0300

    Implementacion de la Barra Superior y del Menu, incorporando imagenes nativas

--- README.md
+++ README.md
@@ -1,9 +1,16 @@
 # AppMusicaUnla
 
 ## Grupo N
+
+## Temática del Desarrollo
+- Aplicacion sobre musica, donde los usuarios pueden registrarse, descubrir canciones y ver los detalles de cada cancion.
+
+## Entrega 1
  - Valentino Villella - GitHub: villella30 - Lista de canciones / Datos completos
  - Santiago Torrens - GitHub: santitorrens1 - Login
  - Thiago Piacentini - GitHub: piacen - Registro de nuevo usuario
 
-## Temática del Desarrollo
- - Aplicacion sobre musica, donde los usuarios pueden registrarse, descubrir canciones y ver los detalles de cada cancion.
+## Entrega 2
+- Valentino Villella - GitHub: villella30 - Funcionamiento de Recordar Usuario con Shared Preferences
+- Santiago Torrens - GitHub: santitorrens1 - Barra superior y Menú
+
--- app/src/main/java/com/example/appmusicagrupon/TerminosYCondicionesActivity.kt
+++ app/src/main/java/com/example/appmusicagrupon/TerminosYCondicionesActivity.kt
@@ -1,16 +1,20 @@
 package com.example.appmusicagrupon
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class TerminosYCondicionesActivity : AppCompatActivity() {
 
     lateinit var btnAceptarTerminos: Button
+
+    lateinit var toolbar: Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +27,11 @@ class TerminosYCondicionesActivity : AppCompatActivity() {
         }
 
         btnAceptarTerminos = findViewById(R.id.btnAceptarTerminos)
+        toolbar = findViewById(R.id.toolbar)
+
+        setSupportActionBar(toolbar)
+        supportActionBar!!.title = resources.getString(R.string.titulo)
+        toolbar.setTitleTextColor(Color.WHITE)
 
         btnAceptarTerminos.setOnClickListener {
 
